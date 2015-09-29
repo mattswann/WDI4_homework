3a19040d9d2b218bca09752c50c28cb32088dd5f
Asif Norzai <asifkhannoorzai@gmail.com>
2015-09-29 19:09:55 +1000

    Changed the iterating part in top_choices.js

--- Asif_Norzai/top_choices.js
+++ Asif_Norzai/top_choices.js
@@ -1,4 +1,6 @@
 var seasons = ['Winter', 'Autumn', 'Spring', 'Summer'];
+var choices = 0;
 for (var i = 0; i < seasons.length; i++) {
-  console.log('My #1 choice is ' + seasons[i]);
+  choices++;
+  console.log('My #' + choices +  ' choice is ' + seasons[i]);
 }
