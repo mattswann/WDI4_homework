9d002636179816ce65c99874e7274658817b0415
Keryn R <kerynjane77@gmail.com>
2015-09-30 17:08:41 +1000

    pt planner js files

---
+++ Keryn_Robinson/PT_planner_30_9/PT_planner2.js
@@ -0,0 +1,39 @@
+// PT planner
+
+var AlameinLine = {
+	origin: 'Flinders Street',
+	destination: 'Glenferrie',
+	stations: ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie']
+}
+
+var GlenWaverleyLine = {
+	origin: 'Flagstaff',
+	destination: 'Tooronga',
+	stations: ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga']
+}
+
+var SandringhamLine = {
+	origin: 'Southern Cross',
+	destination: 'Windsor',
+	stations: ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor']
+}
+
+// var part1 = 'Hawthorn' to 'Richmond'
+
+var part1 = {
+	// stop1: 'Hawthorn' (AlameinLine.stations[4])
+	// stop2: 'Richmond' (AlameinLine.stations[1])
+	console.log(AlameinLine.stations.join [4]-[1];
+}
+
+// var part2 = 'Richmond' to 'Windsor'
+
+var part2 = {
+	stop3: 'Richmond' (SandringhamLine.stations[1])
+	stop4: 'Windsor' (SandringhamLine.stations[4])
+}
+
+function journeyplanner() {
+	// console.log('Hawthorn to Windsor changing at Richmond and displays all stops')
+	console.log('part1' + ' ' + 'part2')
+}
